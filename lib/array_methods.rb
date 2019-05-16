f75081bd0591a0749112eee8bfeb81c0e054bdf0
Justin <sumbhodi1618@gmail.com>
2019-05-16 02:19:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,10 @@
 def using_include(array, element)
+<<<<<<< HEAD
   array.include?(element)
+=======
+  array = []
+    array.include?()
+>>>>>>> 7ca318d784dbaf4924701f6e2700d56d94ecc466
 end
 
 def using_sort(array)
